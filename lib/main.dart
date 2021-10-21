16e430c4afcf1eb72353701c78f0f9ff0dc1a3d1
Abishek <abishekcse78@gmail.com>
2021-10-21 20:31:43 +0530

    mi_card

--- lib/main.dart
+++ lib/main.dart
@@ -12,6 +12,7 @@ class MyCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         body: MiCard(),
       ),
--- lib/mi_card.dart
+++ lib/mi_card.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class MiCard extends StatefulWidget {
@@ -14,15 +15,69 @@ class _MiCardState extends State<MiCard> {
       backgroundColor: Colors.tealAccent,
       body: SafeArea(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: const <Widget>[
             CircleAvatar(
-              radius: 70,
+              radius: 75,
               backgroundColor: Colors.blue,
               backgroundImage: AssetImage('images/abishek.JPG'),
             ),
+            SizedBox(
+              height: 3,
+            ),
             Text(
-              'Abishek IAS',
-              style: TextStyle(fontSize: 25),
+              'ABISHEK T',
+              style: TextStyle(
+                fontSize: 30,
+                fontFamily: 'Pacifico',
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            Text(
+              'Flutter Developer',
+              style: TextStyle(
+                  fontSize: 20,
+                  fontFamily: 'Source Sans Pro',
+                  color: Colors.purple),
+            ),
+            SizedBox(
+              height: 20,
+              width: 150,
+              child: Divider(),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    '+91 7806954817',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
+            ),
+            Card(
+              margin: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
+              color: Colors.white,
+              child: Padding(
+                padding: EdgeInsets.all(10.0),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                  ),
+                  title: Text(
+                    'abishekcse78@gmail.com',
+                    style: TextStyle(fontSize: 20),
+                  ),
+                ),
+              ),
             ),
           ],
         ),
